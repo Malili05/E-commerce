b6a0af174d5929b23c99c0b9e53725b3da8c05bc
Malili05 <malisalitten@gmail.com>
2024-01-03 18:30:32 -0700

    commit for the night

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -125,7 +125,6 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
@@ -174,6 +173,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res
+          .status(404)
+          .json({ message: "There was no product found with this id." });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
